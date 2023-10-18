58bf44fa64cc88ecb9ce51f48f63eedbdb42c33b
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-10-18 12:19:09 -0400

    Exercise 1.4

---
+++ exercises/exercise-1.4.js
@@ -0,0 +1,51 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const addUser = async (request, response) => {
+  const { name } = request.body;
+
+  if (!name) {
+    return response
+      .status(400)
+      .json({ status: 400, message: "Name is required" });
+  }
+
+  const client = new MongoClient(MONGO_URI, options);
+
+  try {
+    await client.connect();
+
+    const db = client.db("exercise_1");
+
+    const result = await db.collection("users").insertOne({ name });
+    
+    // console.log(client);
+    // console.log(db);
+    console.log(result);
+    console.log(result.insertedId);
+    // console.log (name)
+
+    if (result && result.insertedId) {
+      return response
+        .status(200)
+        .json({ status: 200, data: { name: name, _id: result.insertedId } });
+    } else {
+      return response
+        .status(500)
+        .json({ status: 500, message: "Failed to add user" });
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal Server Error" });
+  } finally {
+    client.close();
+  }
+};
+
+module.exports = { addUser };
--- server.js
+++ server.js
@@ -3,21 +3,23 @@
 const express = require("express");
 const morgan = require("morgan");
 const { getUsers } = require("./exercises/exercise-1.3")
+const { addUser } = require("./exercises/exercise-1.4");
 
 
 const PORT = process.env.PORT || 8000;
 
 express()
-    .use(morgan("tiny"))
-    .use(express.static("public"))
-    .use(express.json())
-    .use(express.urlencoded({ extended: false }))
-    .use("/", express.static(__dirname + "/"))
+  .use(morgan("tiny"))
+  .use(express.static("public"))
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use("/", express.static(__dirname + "/"))
+
+  .get("/exercise-1/users", getUsers)
+  .post("/exercise-1/users", addUser)
 
 
-    .get("/exercise-1/users", getUsers)
+  // handle 404s
+  .use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
 
-    // handle 404s
-    .use((req, res) => res.status(404).type("txt").send("🤷‍♂️"))
-
-    .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on port ${PORT}`));
